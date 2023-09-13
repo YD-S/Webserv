63d3da1f08c94a132158a1e488f27cf652f296b2
Ciro García <ciro.garcia@externos.w2m.com>
2023-09-13 19:19:05 +0200

    example stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,17 @@
 
 #include "macros.h"
 
-int main(unused int argc, unused char *argv[]) {
-    LOG_INFO("Hello, World!");
-    return 0;
+int main(int argc, char *argv[]) {
+
+    if (argc != 2) {
+        LOG_ERROR("Usage: ./webserv <config_file>");
+        return 1;
+    }
+
+    LOG_INFO("Starting webserv...");
+    LOG_DEBUG("Reading config file: " << argv[1]);
+    char *config_file = argv[1];
+    (void) config_file;
+
+
 }
