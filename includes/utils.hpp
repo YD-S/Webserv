621000cffda427634541b93399205f1e94eda90c
Ciro García <ciro.garcia@externos.w2m.com>
2023-09-26 09:41:43 +0200

    add utils function for checking if path is a directory

---
+++ includes/utils.hpp
@@ -0,0 +1,12 @@
+//
+// Created by kolterdyx on 26/09/23.
+//
+
+#ifndef WEBSERV_UTILS_HPP
+#define WEBSERV_UTILS_HPP
+
+#include <string>
+
+bool is_dir(const std::string& path);
+
+#endif //WEBSERV_UTILS_HPP
